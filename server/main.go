f0287c92671e663603b799f95b8b207e8f876b69
Jonathan Zheng <jxz12@ic.ac.uk>
2024-01-14 00:29:56 +0000

    still broken with "websocket: RSV1 set, bad opcode 7, bad MASK"

--- server/main.go
+++ server/main.go
@@ -19,10 +19,12 @@ func main() {
     http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
         conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
 
+		fmt.Println("connected")
         for {
             // Read message from browser
             msgType, msg, err := conn.ReadMessage()
             if err != nil {
+				fmt.Println(err)
                 return
             }
 
@@ -34,6 +36,9 @@ func main() {
                 return
             }
         }
+    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "websockets.html")
     })
     http.ListenAndServe(":8080", nil)
 }
